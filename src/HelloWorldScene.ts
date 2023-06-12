34dcc8fdf91dbf3bea558d90a727bfd74824a07d
Min <min.elliot48@gmail.com>
2023-06-12 19:12:25 +1200

    dialogue and moving dog

--- src/HelloWorldScene.ts
+++ src/HelloWorldScene.ts
@@ -1,9 +1,10 @@
 import Phaser from 'phaser'
 
 export default class HelloWorldScene extends Phaser.Scene {
+  private platforms?: Phaser.Physics.Arcade.StaticGroup
   private logo!: Phaser.Physics.Arcade.Image
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-
+  private cat?: Phaser.Physics.Arcade.Sprite
   constructor() {
     super('hello-world')
   }
@@ -13,16 +14,12 @@ export default class HelloWorldScene extends Phaser.Scene {
     this.load.image('livingRoom', 'images/livingroom.jpg')
     this.load.image('Cafe', 'images/cafe.gif')
     this.load.image('logo', 'images/nappyman.png')
-    this.load.spritesheet('mySpriteSheet', 'images/snake.png', {
-      frameWidth: 32,
-      frameHeight: 48,
-    })
+    this.load.image('frame1', 'images/dog/run1.png')
+    this.load.image('frame2', 'images/dog/run1-5.png')
+    this.load.image('frame3', 'images/dog/run2.png')
   }
 
   create() {
-    const x = 5
-    const y = 2
-    const mySprite = this.add.sprite(x, y, 'mySpriteSheet')
     //sizing
     const targetAspectRatio = 30 / 16
     const windowAspectRatio = window.innerWidth / window.innerHeight
@@ -80,6 +77,12 @@ export default class HelloWorldScene extends Phaser.Scene {
     livingRoomBackground.setScale(livingRoomScaleX, livingRoomScaleY)
     cafeBackground.setScale(CafeScaleX, CafeScaleY)
 
+    //player
+    const dialogues = [
+      'Hello there fine sir!',
+      'How are you today????',
+      'Nice to see you I guess...',
+    ]
     this.logo = this.physics.add.image(800, 900, 'logo')
     const logoScaleX = (gameWidth / this.logo.width) * 0.2
     const logoScaleY = (gameHeight / this.logo.height) * 0.4
@@ -89,20 +92,66 @@ export default class HelloWorldScene extends Phaser.Scene {
     this.logo.setCollideWorldBounds(true)
     this.logo.setName('Player')
 
+    this.anims.create({
+      key: 'myAnimation',
+      frames: [{ key: 'frame1' }, { key: 'frame2' }, { key: 'frame3' }],
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    // Play animation on a sprite
+    const mySprite = this.add.sprite(0, 0, 'frame1')
+    mySprite.setPosition(gameWidth / 2, gameHeight / 2)
+    mySprite.setScale(0.1, 0.1)
+    mySprite.play('myAnimation')
+    //make it move
+    this.tweens.add({
+      targets: mySprite,
+      x: gameWidth,
+      duration: 9000,
+      ease: 'linear',
+      repeat: -1,
+      yoyo: true,
+    })
+    //down
+    mySprite.y += 20
+    //left
+    mySprite.x -= 50
+    mySprite.setVisible(false)
+    const dialogueText = this.add.text(0, 0, '')
+    //activating talk
+    this.input.keyboard.on('keydown-C', () => {
+      const distance = Phaser.Math.Distance.Between(
+        mySprite.x,
+        mySprite.y,
+        this.logo.x,
+        this.logo.y
+      )
+      if (distance < 100) {
+        const randomIndex = Math.floor(Math.random() * dialogues.length)
+        const randomDialogue = dialogues[randomIndex]
+        dialogueText.setText(randomDialogue)
+        dialogueText.setPosition(mySprite.x, mySprite.y - 20)
+      }
+    })
+
     this.cursors = this.input.keyboard.createCursorKeys()
     this.input.keyboard.on('keydown-D', () => {
       if (farmBackground.visible) {
         farmBackground.setVisible(false)
         livingRoomBackground.setVisible(true)
         cafeBackground.setVisible(false)
+        mySprite.setVisible(false)
       } else if (livingRoomBackground.visible) {
         farmBackground.setVisible(false)
         livingRoomBackground.setVisible(false)
         cafeBackground.setVisible(true)
+        mySprite.setVisible(true)
       } else {
         farmBackground.setVisible(true)
         livingRoomBackground.setVisible(false)
         cafeBackground.setVisible(false)
+        mySprite.setVisible(false)
       }
     })
   }
